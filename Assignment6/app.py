ed95804db3688bd566c87c5200eb1686ac52c4eb
Jake Swann <jakeswan@usc.edu>
2023-02-19 22:38:03 -0600

    more updates

--- Assignment6/app.py
+++ Assignment6/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import jsonify
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
     return "Hello World!\n"
 
--- Assignment6/static/script.js
+++ Assignment6/static/script.js
@@ -3,23 +3,11 @@ var key = document.getElementById("key");
 var dist = document.getElementById("dist");
 var cat = document.getElementById("cat");
 var location = document.getElementById("location");
+
 let searchBtn = document.getElementById("searchBtn");
 let clearBtn = document.getElementById("clearBtn");
 
-function searchClick();
-function clearClick();
+function insertResults(response){
 
-form.addEventListener("submit",function(event){
-    event.preventDefault();
-})
-
-function searchClick(){
-    
 }
 
-function clearClick(){
-    key.innerHTML = "";
-    dist.innerHTML = "";
-    cat.innerHTML = "Default";
-    location.innerHTML = "";
-}
