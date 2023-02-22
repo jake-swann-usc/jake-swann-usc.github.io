c7042a16c381807593d6d8edcf9ffc8fdca2aa69
Jake Swann <jakeswan@usc.edu>
2023-02-21 19:24:12 -0600

    updates + python

--- Assignment6/static/script.js
+++ Assignment6/static/script.js
@@ -11,12 +11,20 @@ function checkboxEvent(){
 
     if(checkbox.checked){
         location.disabled = true;
+        location.value = "";
+        location.required = false;
     } else {
         location.disabled = false;
+        location.required = true;
     }
 }
 
 function resetForm(){
     var location = document.getElementById("location");
     location.disabled = false;
+    location.value = "";
+    document.getElementById("autoLocate").checked = false;
+    document.getElementById("key").value = "";
+    document.getElementById("dist").value = "";
+    document.getElementById("cat").innerHTML = '<option value="default">Default</option><option value="music">Music</option><option value="sports">Sports</option><option value="arts">Arts</option><option value="theatre">Theatre</option><option value="film">Film</option><option value="misc">Miscellaneous</option>';
 }
