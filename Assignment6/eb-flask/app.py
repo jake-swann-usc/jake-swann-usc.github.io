63043b2fb64be2d90c397315d7e95f92fc092c63
Jake Swann <jakeswan@usc.edu>
2023-02-24 22:19:24 -0600

    mega progress

--- Assignment6/eb-flask/app.py
+++ Assignment6/eb-flask/app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 import requests
 import json
 
+# server url -- http://eventssearch-env.eba-zsuwgjue.us-east-2.elasticbeanstalk.com/
 # ticketmaster api key -- aCWLPVCmdiIXGGhqklGGswfAP32Knqsu
 # ipinfo api key -- d7637bfe3b1b96
 # google api key -- AIzaSyBA3i9a3LPKXS06qtgdPUxHnOFXDxA0Ssg
@@ -13,56 +14,22 @@ app = Flask(__name__)
 def home():
     return app.send_static_file("events.html")
 
-@app.route("/?key=<key>&dist=<dist>&cat=<cat>&autoLocate=on")
+@app.route("/?key=<key>&dist=<dist>&cat=<cat>&loc=<loc>")
 def results():
-    key=request.args.get("key")
-    dist=request.args.get("dist")
-    cat=request.args.get("cat")
-    return key
+    key = request.args.get('key')
+    dist = request.args.get('dist')
+    cat = request.args.get('cat')
+    loc = request.args.get('loc')
 
+    url = f'https://app.ticketmaster.com/discovery/v2/events.json?keyword={key}&geoPoint={loc}&radius={dist}&unit=miles&segmentId={cat}&apikey=aCWLPVCmdiIXGGhqklGGswfAP32Knqsu'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json
+    else:
+        return 'Response Error'
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-# # print a nice greeting.
-# def say_hello(username = "World"):
-#     return '<p>Hello %s!</p>\n' % username
-
-# # some bits of text for the page.
-# header_text = '''
-#     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-# instructions = '''
-#     <p><em>Hint</em>: This is a RESTful web service! Append a username
-#     to the URL (for example: <code>/Thelonious</code>) to say hello to
-#     someone specific.</p>\n'''
-# home_link = '<p><a href="/">Back</a></p>\n'
-# footer_text = '</body>\n</html>'
-
-# # EB looks for an 'application' callable by default.
-# application = Flask(__name__)
-
-# # add a rule for the index page.
-# application.add_url_rule('/', 'index', (lambda: header_text +
-#     say_hello() + instructions + footer_text))
-
-# # add a rule when the page is accessed with a name appended to the site
-# # URL.
-# application.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
-
-# # run the app.
-# if __name__ == "__main__":
-#     # Setting debug to True enables debug output. This line should be
-#     # removed before deploying a production app.
-#     application.debug = True
-#     application.run()
--- Assignment6/eb-flask/static/script.js
+++ Assignment6/eb-flask/static/script.js
@@ -1,28 +1,61 @@
+import Geohash from '/latlon.js'
+
 let searchBtn = document.getElementById("searchBtn");
 let clearBtn = document.getElementById("clearBtn");
 
-var city;
-var region;
-var country;
+var checkbox = document.getElementById("autoLocate");
+var location = document.getElementById("location");
 
-function submitForm(){
+var lat;
+var lon;
+
+const url = "http://eventssearch-env.eba-zsuwgjue.us-east-2.elasticbeanstalk.com/";
+
+async function submitForm(){
+    if(!checkbox.checked){
+        const geo = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location.value + '&key=AIzaSyBA3i9a3LPKXS06qtgdPUxHnOFXDxA0Ssg';
+
+        const request = await fetch(geo);
+        const geoJson = request.json;
+    
+        lat = geoJson.results[0].geometry.location.lat;
+        lon = geoJson.results[0].geometry.location.long;
+    }
+
+    const geohash = Geohash.encode(lat,lon);
+
+    location.value = geohash;
+
+    cat = document.getElementById('cat').value;
+    if(cat=='Music'){
+        cat = 'KZFzniwnSyZfZ7v7nJ';
+    } else if(cat=='Sports'){
+        cat = 'KZFzniwnSyZfZ7v7nE';
+    } else if(cat=='Arts' || cat=='Theatre'){
+        cat = 'KZFzniwnSyZfZ7v7na';
+    } else if(cat=='Film'){
+        cat = 'KZFzniwnSyZfZ7v7nn';
+    } else if(cat=='Miscellaneous'){
+        cat = 'KZFzniwnSyZfZ7v7n1';
+    } else {
+        cat = '';
+    }
+
+    const response = fetch(url + '?key=' + document.getElementById('key').value + '&dist=' + document.getElementById('dist').value + '&cat=' + cat + '&loc=' + geohash);
 
 }
 
 async function checkboxEvent(){
-    var checkbox = document.getElementById("autoLocate");
-    var location = document.getElementById("location");
-
-    var request;
-
     if(checkbox.checked){
         const request = await fetch("https://ipinfo.io/json?token=d7637bfe3b1b96");
         const jsonResponse = await request.json();
 
-        city = jsonResponse.city;
-        region = jsonResponse.region;
-        country = jsonResponse.country;
-        
+        const loc = jsonResponse.loc;
+        const [loclat, loclon] = loc.split(',');
+
+        lat = loclat;
+        lon = loclon;
+
         location.disabled = true;
         location.value = "";
         location.required = false;
@@ -31,6 +64,7 @@ async function checkboxEvent(){
         location.disabled = false;
         location.required = true;
         location.style.display = "block";
+        location.value = "";
     }
 }
 
